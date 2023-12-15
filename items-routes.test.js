83bc0c669aa7643b5d3d347e8a23b58c8d983afa
DD <ddonegal1@gmail.com>
2023-12-15 11:55:58 -0500

    deleted middleware

--- items-routes.test.js
+++ items-routes.test.js
@@ -6,8 +6,8 @@ const app = require("./app");
 let items = require("./fakeDb");
 
 
-const newItem = { name: "popsicle", price: 1.45 }
-const popsicle = { name: 'popsicle', price: 1.45 };
+let newItem = { name: "popsicle", price: 1.45 }
+
 
 
 beforeEach(function () {
@@ -15,7 +15,8 @@ beforeEach(function () {
 });
 
 afterEach(function () {
-    items.length = 0;
+    items = []
+    // items.length = 0;
 });
 
 describe("GET /items", function () {
@@ -26,17 +27,17 @@ describe("GET /items", function () {
     });
 });
 
-describe("GET /item/:name", function () {
+describe("GET /items/:name", function () {
     test("Gets a single item", async function () {
-        const res = await request(app).get(`/item/${popsicle.name}`);
+        const res = await request(app).get(`/items/${newItem.name}`);
         expect(res.status).toBe(200);
+        // expect(res.body).toEqual({newItem});
         expect(res.body).toEqual({ item: popsicle });
-    }, 10000);
+    });
 
     test("Responds with 404 if can't find item", async function () {
         const res = await request(app).get(`/items/0`);
         expect(res.status).toBe(404);
-        debugger;
     });
 });
 
@@ -61,21 +62,22 @@ describe("POST /items", function () {
 
 describe("PATCH /items/:name", function () {
     test("Updates a single item", async () => {
-        const res = await request(app).patch(`/items/${newItem.name}`).send({name: "icecream", price: 1.45});
+        const res = await request(app).patch(`/items/${newItem.name}`).send({ name: "icecream", price: 1.45 });
         expect(res.status).toBe(200);
-        expect(res.body).toEqual({updated: { name: "icecream", price: 1.45 }
+        expect(res.body).toEqual({
+            updated: { name: "icecream", price: 1.45 }
         });
     });
 
-    test("Responds with 404 if id invalid", async() => {
+    test("Responds with 404 if id invalid", async () => {
         const res = await request(app).patch(`/items/0`);
         expect(res.status).toBe(404);
     });
 });
 
 describe("DELETE /items/:name", function () {
-    test("Deletes a single item", async() => {
-        const res = await request(app).delete(`/items/${popsicle.name}`);
+    test("Deletes a single item", async () => {
+        const res = await request(app).delete(`/items/${newItem.name}`);
         expect(res.status).toBe(200);
         expect(res.body).toEqual({ message: "Deleted" });
     });
--- items.js
+++ items.js
@@ -2,10 +2,14 @@ const express = require("express")
 const router = new express.Router()
 const ExpressError = require("./expressError")
 const items = require("./fakeDb");
-const { runInNewContext } = require("vm");
 
-router.get('/', function (req, res) {
-    res.json(items);
+
+router.get('/', function (req, res, next) {
+    try{
+        return  res.json(items);
+    } catch(e) {
+        return next(e)
+    }
 });
 
 router.post('/', function (req, res, next) {
@@ -25,13 +29,38 @@ router.post('/', function (req, res, next) {
     }
 });
 
-router.get("/:name", function (req, res) {
-    const foundItem = items.find(item => item.name === req.params.name)
-    if (foundItem === undefined) {
-        throw new ExpressError("Item not found", 404)
+// router.get("/:name", function (req, res, next) {
+//     try{
+//         const foundItem = items.find(item => item.name === req.params.name)
+//         if (foundItem === undefined) {
+//         throw new ExpressError("Item not found", 404)
+//     }
+//     res.json({ item:foundItem })
+//     } catch(e) {
+//         next(e);
+//     }
+// });
+const items = [
+    { name: "popsicle", price: 1.45 },
+    // Add more items as needed
+  ];
+  
+  // Route to get a single item by name
+  router.get("/:name", (req, res) => {
+    try{
+        const itemName = req.params.name;
+        const foundItem = items.find(item => item.name === itemName);
+        if (!foundItem) {
+            return res.status(404).json({ error: "Item not found" });
     }
-    res.json({ item: foundItem })
-});
+    res.json(foundItem);
+   
+    } catch(e){
+        console.error("Error in GET /:name route:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+      
+  });
 
 router.patch("/:name", function (req, res, next) {
     try {
@@ -59,12 +88,11 @@ router.patch("/:name", function (req, res, next) {
 
 router.delete("/:name", function (req, res, next) {
     try {
-        const foundItem = items.findIndex(item => item.name === req.params.name)
-        if (foundItem === -1) {
+        const foundItem = items.filter(item => item.name === req.params.name)
+        if (foundItem.length === items.length) {
             throw new ExpressError("Item not found", 404)
         }
-        items.splice(foundItem, 1);
-        
+        items= foundItem;
         res.json({ message: "Deleted" })
     } catch (e) {
         next(e);
